099c1c146ceecfccf86feb5812bc27152036d254
Maksym <m.domaretsky@gmail.com>
2017-08-16 13:36:43 +0300

    test

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 
+
 var phraseContainer = document.getElementById('phrase-container');
 var btn = document.getElementById('btn');
 var body = document.getElementById('body');
 var str = document.getElementById('placeholder');
 var author = document.getElementById('author');
+var twitter = document.getElementById('twitter');
+var Placeholdervalue;
 
-  
+
+
+
 
 btn.addEventListener('click', function () {
-     
-       $('#placeholder').hide().fadeIn(1000);
-     $('#author').hide().fadeIn(1500);
-   
-           
+
+    $('#placeholder').hide().fadeIn(1000);
+    $('#author').hide().fadeIn(1500);
+
+
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 'package.json');
     ourRequest.onload = function () {
-      
         var ourData = JSON.parse(ourRequest.responseText);
- 
- 
-        
-        var data = Math.floor(Math.random()* ourData.length);
-        var Placeholdervalue = ourData[data];
-    
-        str.innerHTML =  Placeholdervalue.string ;
+        var data = Math.floor(Math.random() * ourData.length);
+         Placeholdervalue = ourData[data];
+        str.innerHTML = Placeholdervalue.string;
         author.innerHTML = Placeholdervalue.author;
+        
+            var windowObjectReference = 'https://twitter.com/intent/tweet?text=' + Placeholdervalue.string +'' + Placeholdervalue.author;
+        twitter.addEventListener('click', function () {
+                window.open(windowObjectReference, 'twitter');
+            }
+        );
+
     };
     ourRequest.send();
+    
+     
+
 });
+   
 
-
-  
+            
