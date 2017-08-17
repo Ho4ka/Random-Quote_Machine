63c115e92833295b3a57a7a8aad8b8f60bc33725
Maksym <m.domaretsky@gmail.com>
2017-08-17 13:34:55 +0300

    add sharing to twitter button

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-
-
 var phraseContainer = document.getElementById('phrase-container');
 var btn = document.getElementById('btn');
 var body = document.getElementById('body');
 var str = document.getElementById('placeholder');
 var author = document.getElementById('author');
 var twitter = document.getElementById('twitter');
+var shareToTwitter = 'https://twitter.com/intent/tweet?text=' + $('#placeholder').text() + $('#author').text();
+var facebook = document.getElementById('facebook');
+var shareToFacebook = 'https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer';
 var Placeholdervalue;
 
-
-
-
-
-btn.addEventListener('click', function () {
+btn.addEventListener('click', function() {
 
     $('#placeholder').hide().fadeIn(1000);
     $('#author').hide().fadeIn(1500);
 
 
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'package.json');
-    ourRequest.onload = function () {
-        var ourData = JSON.parse(ourRequest.responseText);
+    var quoteRequest = new XMLHttpRequest();
+    quoteRequest.open('GET', 'package.json');
+    quoteRequest.onload = function() {
+        var ourData = JSON.parse(quoteRequest.responseText);
         var data = Math.floor(Math.random() * ourData.length);
-         Placeholdervalue = ourData[data];
+        Placeholdervalue = ourData[data];
         str.innerHTML = Placeholdervalue.string;
         author.innerHTML = Placeholdervalue.author;
-        
-            var windowObjectReference = 'https://twitter.com/intent/tweet?text=' + Placeholdervalue.string +'' + Placeholdervalue.author;
-        twitter.addEventListener('click', function () {
-                window.open(windowObjectReference, 'twitter');
-            }
-        );
+        shareToTwitter = 'https://twitter.com/intent/tweet?text=' + Placeholdervalue.string + '' + Placeholdervalue.author;
+
+
 
     };
-    ourRequest.send();
-    
-     
+    quoteRequest.send();
+
 
 });
-   
 
-            
+twitter.addEventListener('click', function() {
+    window.open(shareToTwitter, 'twitter');
+});
+
+facebook.addEventListener('click', function() {
+    window.open(shareToFacebook, 'facebook');
+});
